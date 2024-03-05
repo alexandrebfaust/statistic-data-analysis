a0989e0ade6768589ed6850b0ece74feb783d326
AlparAlexandreFaust <alexandre.faust@alpar.com.br>
2024-03-04 21:27:22 -0300

    Step 1 Input

---
+++ step1.R
@@ -0,0 +1,37 @@
+setwd("I:/mestrado/statistic-data-analysis")
+
+bubba <- c(3,5,7,9)
+bubba #[1] 3 5 7 9
+bubba[0] #numeric(0)
+bubba[1] #[1] 3
+bubba[2] #[1] 5
+bubba[3] #[1] 7
+bubba[4] #[1] 9
+bubba[5] #[1] NA
+
+heisenberg <- read.csv(file="simple.csv",head=TRUE,sep=",")
+heisenberg
+summary(heisenberg)
+
+help(read.csv)
+
+dir() #[1] "simple.csv" "step1.R"
+getwd() #[1] "I:/mestrado/statistic-data-analysis"
+
+heisenberg$trial #[1] A A B B A B #Levels: A B
+heisenberg$mass #[1] 10.0 11.0  5.0  6.0 10.5  7.0
+heisenberg$velocity #[1] 12 14  8 10 13 11
+
+names(heisenberg) #[1] "trial" "mass" "velocity"
+
+tree <- read.csv(file="trees91.csv",header=TRUE,sep=",");
+attributes(tree)
+names(tree)
+
+tree$C
+
+
+help(read.fwf)
+
+a = read.fwf('fixedWidth.dat',widths=c(-17,15,7),col.names=c('temp','offices'))
+a
